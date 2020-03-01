300d0e153083ae0147ca3719a91f37756d0a8eda
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-01 07:35:12 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY=633728553869558'], ENV['FACEBOOK_SECRET=']
 end
